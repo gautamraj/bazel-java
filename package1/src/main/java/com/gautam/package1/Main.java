b9ac8a1755d024d91242eacf94814d8414eaca6b
Gautam Raj <gautamraj@gmail.com>
2017-06-18 16:47:43 -0700

    Fix Guava dependency

--- package1/src/main/java/com/gautam/package1/Main.java
+++ package1/src/main/java/com/gautam/package1/Main.java
@@ -1,5 +1,7 @@
 package com.gautam.package1;
 
+import com.google.common.base.Joiner;
+
 import com.gautam.package2.Foo;
 
 /**
@@ -9,5 +11,6 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println(Joiner.on(" ").join(new Foo().getFoos()));
   }
 }
--- package2/src/main/java/com/gautam/package2/Foo.java
+++ package2/src/main/java/com/gautam/package2/Foo.java
@@ -1,7 +1,15 @@
 package com.gautam.package2;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
 /**
  * The Foo class
  */
 public class Foo {
+
+  public List<String> getFoos() {
+    return ImmutableList.of("foo1", "foo2", "foo3");
+  }
 }
