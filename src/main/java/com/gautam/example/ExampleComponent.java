bddbb53c024307b050a46397c0a1ede9ee70edce
Gautam Raj <gautamraj@gmail.com>
2020-05-17 22:57:55 -0700

    Fix example

--- src/main/java/com/gautam/example/ExampleComponent.java
+++ src/main/java/com/gautam/example/ExampleComponent.java
@@ -2,8 +2,10 @@ package com.gautam.example;
 
 import com.gautam.package2.Foo;
 import dagger.Component;
+import javax.inject.Singleton;
 
 /** @author Gautam Raj (graj@stripe.com) */
+@Singleton
 @Component(
     modules = {
       ExampleModule.class,
