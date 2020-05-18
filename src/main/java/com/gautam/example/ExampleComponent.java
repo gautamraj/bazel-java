aa1109a06a3e9854118405d0f96728c9e82a0c46
Gautam Raj <gautamraj@gmail.com>
2020-05-17 22:54:29 -0700

    cp

--- src/main/java/com/gautam/example/ExampleComponent.java
+++ src/main/java/com/gautam/example/ExampleComponent.java
@@ -3,10 +3,11 @@ package com.gautam.example;
 import com.gautam.package2.Foo;
 import dagger.Component;
 
-/**
- * @author Gautam Raj (graj@stripe.com)
- */
-@Component
+/** @author Gautam Raj (graj@stripe.com) */
+@Component(
+    modules = {
+      ExampleModule.class,
+    })
 public interface ExampleComponent {
   Foo getFoo();
 }
--- src/main/java/com/gautam/example/ExampleModule.java
+++ src/main/java/com/gautam/example/ExampleModule.java
@@ -2,10 +2,15 @@ package com.gautam.example;
 
 import com.gautam.package2.Foo;
 import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+
 /** @author Gautam Raj (graj@stripe.com) */
 @Module
 public class ExampleModule {
 
+  @Provides
+  @Singleton
   static Foo provideFoo() {
     return new Foo();
   }
